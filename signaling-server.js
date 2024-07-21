0c27fec75d3affffc84b92c4857ae1d378230f4c
tutorials <ganeshdinovator@gmail.com>
2024-07-21 17:41:25 +0530

    Update signaling-server.js

--- signaling-server.js
+++ signaling-server.js
@@ -1,1 +1,16 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        // Broadcast the message to all other clients
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+console.log('Signaling server running on ws://localhost:8080');
 
